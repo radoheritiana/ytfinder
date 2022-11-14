2e3b91d67183485c12525de9a85d85fd1b9c319d
radoheritiana <radoheritiana.rhja.123@gmail.com>
2022-11-14 19:21:03 +0300

    fixing bad url request

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ def tag_finder(url):
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/ytag/api/*": {"origins": "https://ytagfinder.onrender.com"}})
+cors = CORS(app, resources={r"/ytag/api/*": {"origins": "http://localhost:3000"}})
 
 @app.route('/ytag/api/tag_finder', methods=['GET'])
 def get_tag():
 	url = request.args.get('url')
-	tags = {"tags": tag_finder(url)}
-	return jsonify(tags)
+	if "https://www.youtube.com/watch?v=" in url:
+		tags = {"success": True, "tags": tag_finder(url)}
+		return jsonify(tags)
+	else:
+		return jsonify({"success": False, "tags": []})
 
 
 if __name__ == "__main__":
